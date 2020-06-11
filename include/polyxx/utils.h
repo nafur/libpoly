ee126ed2331eaface07fce0c897dd597b1546003
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-11 20:03:17 +0200

    reformatting.

--- include/polyxx/utils.h
+++ include/polyxx/utils.h
@@ -5,8 +5,8 @@
 
 namespace poly {
 
-/** Generic type alias for a unique_ptr with a deleter function. */
-template <typename T>
-using deleting_unique_ptr = std::unique_ptr<T, std::function<void(T*)>>;
+  /** Generic type alias for a unique_ptr with a deleter function. */
+  template <typename T>
+  using deleting_unique_ptr = std::unique_ptr<T, std::function<void(T*)>>;
 
 }  // namespace poly
