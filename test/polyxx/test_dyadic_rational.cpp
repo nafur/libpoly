1ec38bfabb9e536d482063018c2094296b70c22a
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-12 08:29:30 +0200

    Working on rational.

--- test/polyxx/test_dyadic_rational.cpp
+++ test/polyxx/test_dyadic_rational.cpp
@@ -5,8 +5,7 @@
 
 using namespace poly;
 
-TEST_CASE("dyadic_rational::constructors")
-{
+TEST_CASE("dyadic_rational::constructors") {
   CHECK(DyadicRational() == DyadicRational());
   CHECK(DyadicRational() == DyadicRational(0, 1));
   CHECK(DyadicRational(0.0) == DyadicRational(0, 1));
@@ -14,30 +13,26 @@ TEST_CASE("dyadic_rational::constructors")
   CHECK(DyadicRational(2) == DyadicRational(Integer(2)));
 }
 
-TEST_CASE("dyadic_rational::rational")
-{
+TEST_CASE("dyadic_rational::rational") {
   CHECK(Rational(DyadicRational(2)) == Rational(Integer(2)));
 }
 
-TEST_CASE("dyadic_rational::to_double")
-{
+TEST_CASE("dyadic_rational::to_double") {
   CHECK(to_double(DyadicRational(Integer(1))) == 1.0);
   CHECK(to_double(DyadicRational(Integer(2))) == 2.0);
-  CHECK(to_double(DyadicRational(1,2)) == 0.25);
-  CHECK(to_double(DyadicRational(3,2)) == 0.75);
+  CHECK(to_double(DyadicRational(1, 2)) == 0.25);
+  CHECK(to_double(DyadicRational(3, 2)) == 0.75);
 }
 
-TEST_CASE("dyadic_rational::sgn")
-{
+TEST_CASE("dyadic_rational::sgn") {
   CHECK(sgn(DyadicRational(-10)) == -1);
   CHECK(sgn(DyadicRational(-1)) == -1);
-  CHECK(sgn(DyadicRational(0,1)) == 0);
+  CHECK(sgn(DyadicRational(0, 1)) == 0);
   CHECK(sgn(DyadicRational(1)) == 1);
   CHECK(sgn(DyadicRational(10)) == 1);
 }
 
-TEST_CASE("dyadic_rational::operator==")
-{
+TEST_CASE("dyadic_rational::operator==") {
   CHECK_FALSE(DyadicRational(1) == DyadicRational(2));
   CHECK_FALSE(DyadicRational(1) == Integer(2));
   CHECK_FALSE(Integer(1) == DyadicRational(2));
@@ -49,8 +44,7 @@ TEST_CASE("dyadic_rational::operator==")
   CHECK_FALSE(Integer(2) == DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator!=")
-{
+TEST_CASE("dyadic_rational::operator!=") {
   CHECK(DyadicRational(1) != DyadicRational(2));
   CHECK(DyadicRational(1) != Integer(2));
   CHECK(Integer(1) != DyadicRational(2));
@@ -62,8 +56,7 @@ TEST_CASE("dyadic_rational::operator!=")
   CHECK(Integer(2) != DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator<")
-{
+TEST_CASE("dyadic_rational::operator<") {
   CHECK(DyadicRational(1) < DyadicRational(2));
   CHECK(DyadicRational(1) < Integer(2));
   CHECK(Integer(1) < DyadicRational(2));
@@ -75,8 +68,7 @@ TEST_CASE("dyadic_rational::operator<")
   CHECK_FALSE(Integer(2) < DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator<=")
-{
+TEST_CASE("dyadic_rational::operator<=") {
   CHECK(DyadicRational(1) <= DyadicRational(2));
   CHECK(DyadicRational(1) <= Integer(2));
   CHECK(Integer(1) <= DyadicRational(2));
@@ -88,8 +80,7 @@ TEST_CASE("dyadic_rational::operator<=")
   CHECK_FALSE(Integer(2) <= DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator>")
-{
+TEST_CASE("dyadic_rational::operator>") {
   CHECK_FALSE(DyadicRational(1) > DyadicRational(2));
   CHECK_FALSE(DyadicRational(1) > Integer(2));
   CHECK_FALSE(Integer(1) > DyadicRational(2));
@@ -101,8 +92,7 @@ TEST_CASE("dyadic_rational::operator>")
   CHECK(Integer(2) > DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator>=")
-{
+TEST_CASE("dyadic_rational::operator>=") {
   CHECK_FALSE(DyadicRational(1) >= DyadicRational(2));
   CHECK_FALSE(DyadicRational(1) >= Integer(2));
   CHECK_FALSE(Integer(1) >= DyadicRational(2));
@@ -114,8 +104,7 @@ TEST_CASE("dyadic_rational::operator>=")
   CHECK(Integer(2) >= DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::swap")
-{
+TEST_CASE("dyadic_rational::swap") {
   DyadicRational a(1);
   DyadicRational b(2);
   swap(a, b);
@@ -123,66 +112,55 @@ TEST_CASE("dyadic_rational::swap")
   CHECK(b == DyadicRational(1));
 }
 
-TEST_CASE("dyadic_rational::operator+")
-{
+TEST_CASE("dyadic_rational::operator+") {
   CHECK(DyadicRational(1) + DyadicRational(2) == DyadicRational(3));
   CHECK(DyadicRational(1) + Integer(2) == DyadicRational(3));
   CHECK(Integer(1) + DyadicRational(2) == DyadicRational(3));
 }
-TEST_CASE("dyadic_rational::operator-")
-{
+TEST_CASE("dyadic_rational::operator-") {
   CHECK(DyadicRational(1) - DyadicRational(2) == DyadicRational(-1));
   CHECK(-DyadicRational(1) == DyadicRational(-1));
 }
 
-TEST_CASE("dyadic_rational::operator*")
-{
+TEST_CASE("dyadic_rational::operator*") {
   CHECK(DyadicRational(1) * DyadicRational(2) == DyadicRational(2));
 }
-TEST_CASE("dyadic_rational::mul_2exp")
-{
+TEST_CASE("dyadic_rational::mul_2exp") {
   CHECK(mul_2exp(DyadicRational(2), 2) == DyadicRational(8));
 }
-TEST_CASE("dyadic_rational::pow")
-{
+TEST_CASE("dyadic_rational::pow") {
   CHECK(pow(DyadicRational(2), 3) == DyadicRational(8));
 }
 
-TEST_CASE("dyadic_rational::div_2exp")
-{
+TEST_CASE("dyadic_rational::div_2exp") {
   CHECK(div_2exp(DyadicRational(2), 1) == DyadicRational(Integer(1)));
 }
 
-TEST_CASE("dyadic_rational::numerator")
-{
+TEST_CASE("dyadic_rational::numerator") {
   CHECK(numerator(DyadicRational(1)) == Integer(1));
-  CHECK(numerator(DyadicRational(3,4)) == Integer(3));
-  CHECK(numerator(DyadicRational(2,4)) == Integer(1));
+  CHECK(numerator(DyadicRational(3, 4)) == Integer(3));
+  CHECK(numerator(DyadicRational(2, 4)) == Integer(1));
 }
-TEST_CASE("dyadic_rational::denominator")
-{
+TEST_CASE("dyadic_rational::denominator") {
   CHECK(denominator(DyadicRational(1)) == Integer(1));
-  CHECK(denominator(DyadicRational(1,2)) == Integer(4));
+  CHECK(denominator(DyadicRational(1, 2)) == Integer(4));
 }
 
-TEST_CASE("dyadic_rational::is_integer")
-{
+TEST_CASE("dyadic_rational::is_integer") {
   CHECK(is_integer(DyadicRational(1)));
   CHECK(is_integer(DyadicRational(2)));
-  CHECK_FALSE(is_integer(DyadicRational(2,4)));
-  CHECK(is_integer(DyadicRational(4,2)));
-  CHECK_FALSE(is_integer(DyadicRational(3,4)));
+  CHECK_FALSE(is_integer(DyadicRational(2, 4)));
+  CHECK(is_integer(DyadicRational(4, 2)));
+  CHECK_FALSE(is_integer(DyadicRational(3, 4)));
 }
 
-TEST_CASE("dyadic_rational::ceil")
-{
+TEST_CASE("dyadic_rational::ceil") {
   CHECK(ceil(DyadicRational(1)) == Integer(1));
-  CHECK(ceil(DyadicRational(1,3)) == Integer(1));
-  CHECK(ceil(DyadicRational(7,2)) == Integer(2));
+  CHECK(ceil(DyadicRational(1, 3)) == Integer(1));
+  CHECK(ceil(DyadicRational(7, 2)) == Integer(2));
 }
-TEST_CASE("dyadic_rational::floor")
-{
+TEST_CASE("dyadic_rational::floor") {
   CHECK(floor(DyadicRational(1)) == Integer(1));
-  CHECK(floor(DyadicRational(1,3)) == Integer(0));
-  CHECK(floor(DyadicRational(7,2)) == Integer(1));
+  CHECK(floor(DyadicRational(1, 3)) == Integer(0));
+  CHECK(floor(DyadicRational(7, 2)) == Integer(1));
 }
--- test/polyxx/test_rational.cpp
+++ test/polyxx/test_rational.cpp
@@ -5,20 +5,21 @@
 
 using namespace poly;
 
-TEST_CASE("rational::constructors")
-{
+TEST_CASE("rational::constructors") {
   CHECK(Rational() == Rational());
   CHECK(Rational() == Rational(0, 1));
+  CHECK(Rational(13) == Rational(26, 2));
+  CHECK(Rational(Integer(7)) == Rational(21, 3));
+  CHECK(Rational(3.0) == Rational(6, 2));
 }
 
-TEST_CASE("rational::to_double")
-{
+TEST_CASE("rational::to_double") {
+  CHECK(to_double(Rational(1, 2)) == 0.5);
   CHECK(to_double(Rational(1)) == 1.0);
   CHECK(to_double(Rational(2)) == 2.0);
 }
 
-TEST_CASE("rational::sgn")
-{
+TEST_CASE("rational::sgn") {
   CHECK(sgn(Rational(-10)) == -1);
   CHECK(sgn(Rational(-1)) == -1);
   CHECK(sgn(Rational()) == 0);
@@ -26,8 +27,7 @@ TEST_CASE("rational::sgn")
   CHECK(sgn(Rational(10)) == 1);
 }
 
-TEST_CASE("rational::operator==")
-{
+TEST_CASE("rational::operator==") {
   CHECK_FALSE(Rational(1) == Rational(2));
   CHECK_FALSE(Rational(1) == Integer(2));
   CHECK_FALSE(Integer(1) == Rational(2));
@@ -39,8 +39,7 @@ TEST_CASE("rational::operator==")
   CHECK_FALSE(Integer(2) == Rational(1));
 }
 
-TEST_CASE("rational::operator!=")
-{
+TEST_CASE("rational::operator!=") {
   CHECK(Rational(1) != Rational(2));
   CHECK(Rational(1) != Integer(2));
   CHECK(Integer(1) != Rational(2));
@@ -52,8 +51,7 @@ TEST_CASE("rational::operator!=")
   CHECK(Integer(2) != Rational(1));
 }
 
-TEST_CASE("rational::operator<")
-{
+TEST_CASE("rational::operator<") {
   CHECK(Rational(1) < Rational(2));
   CHECK(Rational(1) < Integer(2));
   CHECK(Integer(1) < Rational(2));
@@ -65,8 +63,7 @@ TEST_CASE("rational::operator<")
   CHECK_FALSE(Integer(2) < Rational(1));
 }
 
-TEST_CASE("rational::operator<=")
-{
+TEST_CASE("rational::operator<=") {
   CHECK(Rational(1) <= Rational(2));
   CHECK(Rational(1) <= Integer(2));
   CHECK(Integer(1) <= Rational(2));
@@ -78,8 +75,7 @@ TEST_CASE("rational::operator<=")
   CHECK_FALSE(Integer(2) <= Rational(1));
 }
 
-TEST_CASE("rational::operator>")
-{
+TEST_CASE("rational::operator>") {
   CHECK_FALSE(Rational(1) > Rational(2));
   CHECK_FALSE(Rational(1) > Integer(2));
   CHECK_FALSE(Integer(1) > Rational(2));
@@ -91,8 +87,7 @@ TEST_CASE("rational::operator>")
   CHECK(Integer(2) > Rational(1));
 }
 
-TEST_CASE("rational::operator>=")
-{
+TEST_CASE("rational::operator>=") {
   CHECK_FALSE(Rational(1) >= Rational(2));
   CHECK_FALSE(Rational(1) >= Integer(2));
   CHECK_FALSE(Integer(1) >= Rational(2));
@@ -104,8 +99,7 @@ TEST_CASE("rational::operator>=")
   CHECK(Integer(2) >= Rational(1));
 }
 
-TEST_CASE("rational::swap")
-{
+TEST_CASE("rational::swap") {
   Rational a(1);
   Rational b(2);
   swap(a, b);
@@ -113,71 +107,59 @@ TEST_CASE("rational::swap")
   CHECK(b == Rational(1));
 }
 
-TEST_CASE("rational::operator+")
-{
+TEST_CASE("rational::operator+") {
   CHECK(Rational(1) + Rational(2) == Rational(3));
   CHECK(Rational(1) + Integer(2) == Rational(3));
   CHECK(Integer(1) + Rational(2) == Rational(3));
 }
-TEST_CASE("rational::operator-")
-{
+TEST_CASE("rational::operator-") {
   CHECK(Rational(1) - Rational(2) == Rational(-1));
   CHECK(-Rational(1) == Rational(-1));
 }
 
-TEST_CASE("rational::inverse")
-{
+TEST_CASE("rational::inverse") {
   CHECK(inverse(Rational(1)) == Rational(1));
   CHECK(inverse(Rational(Integer(1), Integer(2))) == Rational(2));
   CHECK(inverse(Rational(2)) == Rational(Integer(1), Integer(2)));
 }
-TEST_CASE("rational::operator*")
-{
+TEST_CASE("rational::operator*") {
   CHECK(Rational(1) * Rational(2) == Rational(2));
 }
-TEST_CASE("rational::mul_2exp")
-{
+TEST_CASE("rational::mul_2exp") {
   CHECK(mul_2exp(Rational(2), 2) == Rational(8));
 }
 TEST_CASE("rational::pow") { CHECK(pow(Rational(2), 3) == Rational(8)); }
-TEST_CASE("rational::operator/")
-{
+TEST_CASE("rational::operator/") {
   CHECK(Rational(1) / Rational(1) == Rational(1));
   CHECK(Rational(1) / Rational(2) == Rational(Integer(1), Integer(2)));
 }
 
-TEST_CASE("rational::div_2exp")
-{
+TEST_CASE("rational::div_2exp") {
   CHECK(div_2exp(Rational(2), 2) == Rational(Integer(1), Integer(2)));
 }
 
-TEST_CASE("rational::numerator")
-{
+TEST_CASE("rational::numerator") {
   CHECK(numerator(Rational(1)) == Rational(1));
   CHECK(numerator(Rational(Integer(1), Integer(2))) == Rational(1));
   CHECK(numerator(Rational(2)) == Rational(2));
 }
-TEST_CASE("rational::denominator")
-{
+TEST_CASE("rational::denominator") {
   CHECK(denominator(Rational(1)) == Rational(1));
   CHECK(denominator(Rational(Integer(1), Integer(2))) == Rational(2));
   CHECK(denominator(Rational(2)) == Rational(1));
 }
 
-TEST_CASE("rational::is_integer")
-{
+TEST_CASE("rational::is_integer") {
   CHECK(is_integer(Rational(1)));
   CHECK_FALSE(is_integer(Rational(Integer(1), Integer(2))));
   CHECK(is_integer(Rational(2)));
 }
 
-TEST_CASE("rational::ceil")
-{
+TEST_CASE("rational::ceil") {
   CHECK(ceil(Rational(1)) == Rational(1));
   CHECK(ceil(Rational(Integer(1), Integer(2))) == Rational(1));
 }
-TEST_CASE("rational::floor")
-{
+TEST_CASE("rational::floor") {
   CHECK(floor(Rational(1)) == Rational(1));
   CHECK(floor(Rational(Integer(1), Integer(2))) == Rational());
 }
