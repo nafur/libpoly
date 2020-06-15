317477126fed2cc91324f69d1d99ebb261a90146
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-15 12:40:04 +0200

    Implement count_real_roots.

--- src/polyxx/upolynomial_roots.cpp
+++ src/polyxx/upolynomial_roots.cpp
@@ -2,6 +2,10 @@
 
 namespace poly {
 
+std::size_t count_real_roots(const UPolynomial& p, const RationalInterval& ri) {
+  return lp_upolynomial_roots_count(p.get_internal(), ri.get_internal());
+}
+
 std::vector<AlgebraicNumber> isolate_real_roots(const UPolynomial& p) {
   lp_algebraic_number_t* roots = new lp_algebraic_number_t[degree(p)];
   std::size_t roots_size;
