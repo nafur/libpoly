5c0047a46fecb9292eda93284264a61818902ad5
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-17 22:24:40 +0200

    Working on interval assignment.

--- include/polyxx/interval_assignment.h
+++ include/polyxx/interval_assignment.h
@@ -10,39 +10,41 @@
 
 namespace poly {
 
-/**
- * Implements a wrapper for lp_interval_assignment_t.
- */
-class IntervalAssignment {
-  /** The actual assignment. */
-  lp_interval_assignment_t mAssignment;
-
- public:
-  /** Construct an empty assignment with a custom context. */
-  IntervalAssignment(const Context& c);
-  /** Construct an empty assignment. */
-  IntervalAssignment() : IntervalAssignment(Context::get_context()) {}
-  /** Custom destructor. */
-  ~IntervalAssignment();
-
-  /** Get a non-const pointer to the internal lp_interval_assignment_t. Handle
-   * with care!
+  /**
+   * Implements a wrapper for lp_interval_assignment_t.
    */
-  lp_interval_assignment_t* get_internal();
-  /** Get a const pointer to the internal lp_interval_assignment_t. */
-  const lp_interval_assignment_t* get_internal() const;
+  class IntervalAssignment {
+    /** The actual assignment. */
+    lp_interval_assignment_t mAssignment;
 
-  /** Assign var to the given value. */
-  void set(const Variable& var, const Interval& interval);
-  /** Unassign the given variable. */
-  void unset(const Variable& var);
-  /** Retrieve a value from the Assignment. */
-  Interval get(const Variable& var) const;
-  /** Clear the assignment. */
-  void clear();
-};
+   public:
+    /** Construct an empty assignment with a custom context. */
+    IntervalAssignment(const Context& c);
+    /** Construct an empty assignment. */
+    IntervalAssignment();
+    /** Custom destructor. */
+    ~IntervalAssignment();
 
-/** Stream the given Assignment to an output stream. */
-std::ostream& operator<<(std::ostream& os, const IntervalAssignment& a);
+    /** Get a non-const pointer to the internal lp_interval_assignment_t. Handle
+     * with care!
+     */
+    lp_interval_assignment_t* get_internal();
+    /** Get a const pointer to the internal lp_interval_assignment_t. */
+    const lp_interval_assignment_t* get_internal() const;
+
+    /** Assign var to the given interval. */
+    void set(const Variable& var, const Interval& interval);
+    /** Unassign the given variable. */
+    void unset(const Variable& var);
+    /** Check whether the variable has an interval (that is not the full interval). */
+    bool has(const Variable& var) const;
+    /** Retrieve an interval from the Assignment. */
+    Interval get(const Variable& var) const;
+    /** Clear the assignment. */
+    void clear();
+  };
+
+  /** Stream the given Assignment to an output stream. */
+  std::ostream& operator<<(std::ostream& os, const IntervalAssignment& a);
 
 }  // namespace poly
