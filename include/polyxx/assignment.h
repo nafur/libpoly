519cb36519855dd142745972f791404311a5a64a
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-17 22:48:39 +0200

    Reformatting assignment.

--- include/polyxx/assignment.h
+++ include/polyxx/assignment.h
@@ -9,40 +9,41 @@
 
 namespace poly {
 
-/**
- * Implements a wrapper for lp_assignment_t.
- */
-class Assignment {
-  /** The actual assignment. */
-  lp_assignment_t mAssignment;
-
- public:
-  /** Construct an empty assignment with a custom context. */
-  Assignment(const Context& c);
-  /** Construct an empty assignment. */
-  Assignment() : Assignment(Context::get_context()) {}
-  /** Custom destructor. */
-  ~Assignment();
-
-  /** Get a non-const pointer to the internal lp_assignment_t. Handle with care!
+  /**
+   * Implements a wrapper for lp_assignment_t.
    */
-  lp_assignment_t* get_internal();
-  /** Get a const pointer to the internal lp_assignment_t. */
-  const lp_assignment_t* get_internal() const;
+  class Assignment {
+    /** The actual assignment. */
+    lp_assignment_t mAssignment;
 
-  /** Assign variable to the given value. */
-  void set(const Variable& var, const Value& value);
-  /** Unassign the given variable. */
-  void unset(const Variable& var);
-  /** Check whether the variable is assigned. */
-  bool has(const Variable& var) const;
-  /** Retrieve a value from the Assignment. */
-  const Value& get(const Variable& var) const;
-  /** Clear the assignment. */
-  void clear();
-};
+   public:
+    /** Construct an empty assignment with a custom context. */
+    Assignment(const Context& c);
+    /** Construct an empty assignment. */
+    Assignment() : Assignment(Context::get_context()) {}
+    /** Custom destructor. */
+    ~Assignment();
 
-/** Stream the given Assignment to an output stream. */
-std::ostream& operator<<(std::ostream& os, const Assignment& a);
+    /** Get a non-const pointer to the internal lp_assignment_t. Handle with
+     * care!
+     */
+    lp_assignment_t* get_internal();
+    /** Get a const pointer to the internal lp_assignment_t. */
+    const lp_assignment_t* get_internal() const;
+
+    /** Assign variable to the given value. */
+    void set(const Variable& var, const Value& value);
+    /** Unassign the given variable. */
+    void unset(const Variable& var);
+    /** Check whether the variable is assigned. */
+    bool has(const Variable& var) const;
+    /** Retrieve a value from the Assignment. */
+    const Value& get(const Variable& var) const;
+    /** Clear the assignment. */
+    void clear();
+  };
+
+  /** Stream the given Assignment to an output stream. */
+  std::ostream& operator<<(std::ostream& os, const Assignment& a);
 
 }  // namespace poly
