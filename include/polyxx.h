7e8ea8d6054722396fa90070395fc9759aa1cc59
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-18 10:20:31 +0200

    Merge upolynomial_roots stuff into upolynomial.

--- include/polyxx.h
+++ include/polyxx.h
@@ -48,6 +48,5 @@ This also allows for direct conversions between std::vector<Data>::data() and lp
 #include "polyxx/rational_interval.h"
 #include "polyxx/sign_condition.h"
 #include "polyxx/upolynomial.h"
-#include "polyxx/upolynomial_roots.h"
 #include "polyxx/value.h"
 #include "polyxx/variable.h"
