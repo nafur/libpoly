2859728e9381216f65c22e0e23d8902951bae4b4
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2020-06-18 10:10:06 +0200

    Adding tests for upolynomial_roots.

--- include/polyxx/upolynomial_roots.h
+++ include/polyxx/upolynomial_roots.h
@@ -6,9 +6,13 @@
 
 namespace poly {
 
-std::size_t count_real_roots(const UPolynomial& p, const RationalInterval& ri);
+  /** Count the real roots of a polynomial within a rational interval.*/
+  std::size_t count_real_roots(const UPolynomial& p,
+                               const RationalInterval& ri);
 
-/** Isolate the real roots of a UPolynomial. */
-std::vector<AlgebraicNumber> isolate_real_roots(const UPolynomial& p);
+  /** Isolate the real roots of a UPolynomial, returning them as algebraic
+   * numbers. The roots are sorted in increasing numerical order.
+   */
+  std::vector<AlgebraicNumber> isolate_real_roots(const UPolynomial& p);
 
 }  // namespace poly
